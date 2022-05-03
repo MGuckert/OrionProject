91f11171ec43d275f6c8d9382fbe363edb4a33ff
ThibaudPiccinali <100284370+ThibaudPiccinali@users.noreply.github.com>
2022-05-03 16:48:47 +0200

    Feat : Rajout du bouton (On/Off) Bluetooth en bas a droite.

--- app/src/main/java/com/example/tactigant20/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/tactigant20/ui/home/HomeViewModel.java
@@ -4,6 +4,8 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.tactigant20.R;
+
 public class HomeViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
