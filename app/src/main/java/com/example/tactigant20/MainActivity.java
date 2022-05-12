dd004c2b838ce51c08ea5fa5950e0c58eaca4cc8
ThibaudPiccinali <100284370+ThibaudPiccinali@users.noreply.github.com>
2022-05-12 23:50:00 +0200

    Feat : Ajout de la notification (première version basique)

--- app/src/main/java/com/example/tactigant20/MainActivity.java
+++ app/src/main/java/com/example/tactigant20/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.tactigant20;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
 import android.media.Image;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
@@ -12,6 +16,8 @@ import com.example.tactigant20.ui.settings.SettingsMain;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -25,14 +31,14 @@ public class MainActivity extends AppCompatActivity {
     private ImageView settingsIcon;
     private ImageView helpIcon;
     private ImageView infoIcon;
-
+    private NotificationCompat.Builder builder;
     private static final String TAG_MAIN = "DebugMainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(TAG_MAIN,"Appel de onCreate dans MainActivity");
         super.onCreate(savedInstanceState);
-
+        createNotificationChannel();
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
@@ -55,8 +61,42 @@ public class MainActivity extends AppCompatActivity {
         //Information button on toolbar
         infoIcon = findViewById(R.id.infoicon);
         infoIcon.setOnClickListener(view -> openInfo());
+
+        // Paramètres de la notification
+        builder = new NotificationCompat.Builder(this, "CHANNEL_ID")
+                .setSmallIcon(R.drawable.ic_home_black_24dp)
+                .setContentTitle("Status du bracelet : CONNECTED/DISCONNECTED")
+                .setContentText("Batterie: 50 %")
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
+
+        // Fais apparaitre la notification
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(100, builder.build());
     }
 
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Tactigant channel";
+            String description = "Tactigant Channel description";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("CHANNEL_ID", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    public static void cancelNotification(Context ctx, int notifyId) {
+        // Permet de supprimer la notif. Mettre ctx=this et notifyId=100
+        String ns = Context.NOTIFICATION_SERVICE;
+        NotificationManager nMgr = (NotificationManager) ctx.getSystemService(ns);
+        nMgr.cancel(notifyId);
+    }
     private void openSettings() {
         Log.d(TAG_MAIN,"appel de openSettings dans MainActivity");
         Intent intent = new Intent(this, SettingsMain.class);
