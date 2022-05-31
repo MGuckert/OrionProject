63effec1377a7344367593d55bcbadf8b39f69d9
Lucas SALAND <lucassld700@gmail.com>
2022-05-31 09:47:01 +0200

    feat: implemented MyNotificationListenerService.java

---
+++ app/src/main/java/com/example/tactigant20/MyNotificationListenerService.java
@@ -0,0 +1,20 @@
+package com.example.tactigant20;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+
+public class MyNotificationListenerService extends NotificationListenerService {
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
+        super.onNotificationPosted(sbn);
+    }
+
+    @Override
+    public void onNotificationRemoved(StatusBarNotification sbn) {
+        super.onNotificationRemoved(sbn);
+    }
+}
