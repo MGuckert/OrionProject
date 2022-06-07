2c2f6228c0706c15dd1534d05235f1bea715e7c1
Lucas SALAND <lucassld700@gmail.com>
2022-06-07 11:03:15 +0200

    feat : + de Log.i dans onNotificationPosted

--- app/src/main/java/com/example/tactigant20/MyNotificationListenerService.java
+++ app/src/main/java/com/example/tactigant20/MyNotificationListenerService.java
@@ -1,5 +1,6 @@
 package com.example.tactigant20;
 
+import android.app.Notification;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -8,6 +9,7 @@ import android.os.IBinder;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
+import android.widget.Toast;
 
 //pour que cela fonctionne correctement il semble qu'il faille autoriser l'application à obtenir toutes les notifications
 //pour cela taper dans la barre de recherche des paramètres android "accéder aux notifications"
@@ -24,8 +26,12 @@ public class MyNotificationListenerService extends NotificationListenerService {
     public void onNotificationPosted(StatusBarNotification sbn) {
         super.onNotificationPosted(sbn);
         String packageName = sbn.getPackageName();
+        Notification notif = sbn.getNotification();
+        String category = notif.category;
         Log.d(TAG, "notificationPosted");
         Log.i(TAG, "package name : "+packageName);
+        Log.i(TAG,"notification : "+notif);
+        Log.i(TAG,"category : "+category);
     }
 
     @Override
