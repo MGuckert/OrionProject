8bc9a475e94ec0aa2ae42330b8d60316b219c4c9
ThibaudPiccinali <100284370+ThibaudPiccinali@users.noreply.github.com>
2022-06-10 16:29:41 +0200

    Feat : Scanning for devices ble (première version ne marche pas trop)

--- app/src/main/java/com/example/tactigant20/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/tactigant20/ui/home/HomeFragment.java
@@ -4,8 +4,15 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.ParcelUuid;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,12 +30,19 @@ import com.example.tactigant20.R;
 import com.example.tactigant20.databinding.FragmentHomeBinding;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class HomeFragment extends Fragment {
     private FragmentHomeBinding binding;
     private Button BluetoothButton;
-    // private BluetoothAdapter mBlueAdapter;
+    private Button BluetoothSettingsButton;
+    private TextView TextedeChargement;
 
     private static final String TAG_HOME = "DebugHomeFragment";
+    private BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+    private BluetoothLeScanner scanner = adapter.getBluetoothLeScanner();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -53,8 +67,11 @@ public class HomeFragment extends Fragment {
         // Bouton bluetooth
         BluetoothButton=root.findViewById(R.id.bluetoothButton);
         BluetoothButton.setOnClickListener(BluetoothButtonListener);
-        // mBlueAdapter=BluetoothAdapter.getDefaultAdapter();
+        BluetoothSettingsButton=root.findViewById(R.id.ScanBouton);
+        BluetoothSettingsButton.setOnClickListener(BluetoothButtonListener);
 
+        // Texte de chargement
+        TextedeChargement = root.findViewById(R.id.TextedeChargement);
         return root;
     }
 
@@ -62,12 +79,50 @@ public class HomeFragment extends Fragment {
     private View.OnClickListener BluetoothButtonListener = new View.OnClickListener() {
 
         @Override
-        public void onClick(View view) {
-            Log.d(TAG_HOME,"Appui sur le bouton Bluetooth");
-            Intent intentOpenBluetoothSettings = new Intent();
-            intentOpenBluetoothSettings.setAction(android.provider.Settings.ACTION_BLUETOOTH_SETTINGS);
-            startActivity(intentOpenBluetoothSettings);
-        }
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.ScanBouton:
+                    TextedeChargement.setVisibility(View.VISIBLE);
+                    if(!adapter.isEnabled()){
+                        Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                        startActivityForResult(intent,0);
+                    }
+                    // Filtre
+                        if (scanner != null) {
+                            UUID BLP_SERVICE_UUID = UUID.fromString("00001810-0000-1000-8000-00805f9b34fb"); // A changer en fonction de celui de la carte
+                            UUID[] serviceUUIDs = new UUID[]{BLP_SERVICE_UUID};
+                            List<ScanFilter> filters = null;
+                            if(serviceUUIDs != null) {
+                                filters = new ArrayList<>();
+                                for (UUID serviceUUID : serviceUUIDs) {
+                                    ScanFilter filter = new ScanFilter.Builder()
+                                            .setServiceUuid(new ParcelUuid(serviceUUID))
+                                            .build();
+                                    filters.add(filter);
+                                }
+                            }
+                        //Type de scan
+                            ScanSettings scanSettings = new ScanSettings.Builder()
+                                    .setScanMode(ScanSettings.SCAN_MODE_LOW_POWER)
+                                    .setCallbackType(ScanSettings.CALLBACK_TYPE_ALL_MATCHES)
+                                    .setMatchMode(ScanSettings.MATCH_MODE_AGGRESSIVE)
+                                    .setNumOfMatches(ScanSettings.MATCH_NUM_ONE_ADVERTISEMENT)
+                                    .setReportDelay(0L)
+                                    .build();
+                        //scanner.startScan(filters, scanSettings, scanCallback);
+                        Log.d("Bluetooth", "scan started");
+                    }  else {
+                        Log.e("Bluetooth", "could not get scanner object");
+                    }
+                    break;
+                case R.id.bluetoothButton:
+                    Log.d(TAG_HOME, "Appel de onClick dans HomeFragment");
+                    Intent intentOpenBluetoothSettings = new Intent();
+                    intentOpenBluetoothSettings.setAction(android.provider.Settings.ACTION_BLUETOOTH_SETTINGS);
+                    startActivity(intentOpenBluetoothSettings);
+                    break;
+            }
+        };
     };
 
     @Override
@@ -80,4 +135,22 @@ public class HomeFragment extends Fragment {
         Log.d(TAG_HOME,"Appel de alerter dans MenuFragment");
         Toast.makeText(getActivity(),s,Toast.LENGTH_LONG).show();
     }
+    private final ScanCallback scanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            BluetoothDevice device = result.getDevice();
+            // ...do whatever you want with this found device
+            TextedeChargement.setVisibility(View.INVISIBLE);
+        }
+
+        @Override
+        public void onBatchScanResults(List<ScanResult> results) {
+            // Ignore for now
+        }
+
+        @Override
+        public void onScanFailed(int errorCode) {
+            // Ignore for now
+        }
+    };
 }
