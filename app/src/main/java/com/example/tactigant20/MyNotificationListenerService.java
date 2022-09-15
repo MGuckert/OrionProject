4545160a3579b6d9e0a227be853688fc7c00f0eb
Lucas SALAND <lucassld700@gmail.com>
2022-09-15 16:28:22 +0200

    feat : changement implémentation de loadVibrationMode devenue méthode public static + nouvel argument (Context context) pour pouvoir être utilisée dans d'autre classe, notamment MyNotificationListenerService

--- app/src/main/java/com/example/tactigant20/MyNotificationListenerService.java
+++ app/src/main/java/com/example/tactigant20/MyNotificationListenerService.java
@@ -7,6 +7,16 @@ import android.service.notification.StatusBarNotification;
 import android.util.Log;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.example.tactigant20.ui.notifications.NotificationsFragment;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 //pour que cela fonctionne correctement il semble qu'il faille autoriser l'application à obtenir toutes les notifications
 //pour cela taper dans la barre de recherche des paramètres android "accéder aux notifications"
 public class MyNotificationListenerService extends NotificationListenerService {
@@ -24,9 +34,10 @@ public class MyNotificationListenerService extends NotificationListenerService {
         String packageName = sbn.getPackageName();
         Notification notif = sbn.getNotification();
         String category = notif.category;
+        String vibrationMode = NotificationsFragment.loadVibrationMode(packageName, getApplicationContext());
         if(category != null)
             if (!category.equals("sys"))//attention risque de NullPointerException !!!
-                showToast("Notification reçu : "+packageName);
+                showToast("Notification reçu : "+packageName + " Vibration mode : " + vibrationMode);
         Log.d(TAG_MNLS, "notificationPosted");
         Log.i(TAG_MNLS, "package name : "+packageName);
         Log.i(TAG_MNLS,"notification : "+notif);
@@ -38,7 +49,40 @@ public class MyNotificationListenerService extends NotificationListenerService {
         super.onNotificationRemoved(sbn);
     }
 
-    public void showToast(String msg){
-        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
-    }
+    public void showToast(String msg){Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();}
+
+    //l'implémentation fournie par NotificationsFragment n'est pas utilisable car la méthode est privée et pas statique
+    //La méthode à également besoin du contexte
+    /*private String loadVibrationMode(String notifName) {
+        //Fonction renvoyant le mode de vibration de l'application qui a pour package "notifName" sauvegardé dans le fichier
+        // "vibration_modes_data.txt", et "UNKNOWN" si aucune donnée pour cette application n'a été sauvegardée.
+        FileInputStream inputStream = null;
+        try {
+            inputStream = getApplicationContext().openFileInput("vibration_modes_data.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (inputStream != null) {
+            InputStreamReader inputReader = new InputStreamReader(inputStream);
+            BufferedReader buffReader = new BufferedReader(inputReader);
+
+            String line = null;
+            do { //On lit le fichier ligne par ligne, en comparant le début de chaque ligne avec "notifName" :
+                // s'il est identique, on est sur la bonne ligne, et on peut renvoyer le mode de vibration écrit !
+                try {
+                    line = buffReader.readLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.err.println(line);
+                int n = notifName.length();
+                if ( line != null && n < line.length()) {
+                    if (line.substring(0, n).equals(notifName)) {
+                        return line.substring(line.length()-1, line.length());
+                    }
+                }
+            } while (line != null);
+        }
+        return "UNKNOWN";
+    }*/
 }
