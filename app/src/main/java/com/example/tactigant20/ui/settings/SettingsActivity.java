0f9e109289c6480a6fff4d6ecdbbb79ccb6841c4
Roman TIEDREZ <98756662+RTiedrez@users.noreply.github.com>
2022-12-12 14:58:48 +0100

    Fix: typo in strings.xml

--- app/src/main/java/com/example/tactigant20/ui/settings/SettingsActivity.java
+++ app/src/main/java/com/example/tactigant20/ui/settings/SettingsActivity.java
@@ -1,19 +1,43 @@
 package com.example.tactigant20.ui.settings;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.tactigant20.R;
 
+import java.util.regex.Pattern;
+
 public class SettingsActivity extends AppCompatActivity {
 
     private static final String TAG_SETTINGS = "debug_settings_activity";
+
+    private EditText settingsEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        settingsEditText = findViewById(R.id.settings_editText);
+
+        Button settingsButton = findViewById(R.id.settings_button);
+        settingsButton.setOnClickListener(this::cSettingsButton);
+
+    }
+
+    private void cSettingsButton(View v) {
+        String MACTemp = settingsEditText.getText().toString().toUpperCase();
+        if (Pattern.matches("^([0-9A-F]{2}[:-]){5}([0-9A-F]{2})$", MACTemp)) {
+            settingsEditText.setText(MACTemp);
+            Toast.makeText(this, "Nouvelle adresse MAC : " + MACTemp, Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, getString(R.string.MAC_error), Toast.LENGTH_SHORT).show();
+        }
     }
 }
