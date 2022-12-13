6b3dcefe0eb51a6538dc37616c9e3a2b242f3602
Mathis Guckert <guckert.mathis@gmail.com>
2022-12-13 17:16:45 +0100

    Feat : added a switch in the SettingsFragment that can apply  dark mode. The selected mode is saved in the preferences file

--- app/src/main/java/com/example/tactigant20/ui/settings/SettingsActivity.java
+++ app/src/main/java/com/example/tactigant20/ui/settings/SettingsActivity.java
@@ -1,16 +1,23 @@
 package com.example.tactigant20.ui.settings;
 
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import com.example.tactigant20.MainActivity;
 import com.example.tactigant20.R;
+import com.example.tactigant20.ui.fragments.HomeFragment;
 
 import java.util.regex.Pattern;
 
@@ -32,6 +39,18 @@ public class SettingsActivity extends AppCompatActivity {
         Button settingsButton = findViewById(R.id.settings_button);
         settingsButton.setOnClickListener(this::cSettingsButton);
 
+
+        //Switch mode sombre
+        Switch darkModeSwitch = findViewById(R.id.dark_mode_switch);
+
+        darkModeSwitch.setChecked(isDarkModeEnabled());
+        darkModeSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                enableDarkMode();
+            } else {
+                disableDarkMode();
+            }
+        });
     }
 
     private void cSettingsButton(View v) {
@@ -45,5 +64,28 @@ public class SettingsActivity extends AppCompatActivity {
         } else {
             Toast.makeText(this, getString(R.string.MAC_error), Toast.LENGTH_SHORT).show();
         }
+    }
+
+    // Méthode pour vérifier si le mode sombre est activé
+    private boolean isDarkModeEnabled() {
+        return (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES);
+    }
+
+    // Méthode pour activer le mode sombre
+    private void enableDarkMode() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        getDelegate().applyDayNight();
+        SharedPreferences preferences =
+                PreferenceManager.getDefaultSharedPreferences(this);
+        preferences.edit().putInt("night_mode", AppCompatDelegate.MODE_NIGHT_YES).apply();
+    }
+
+    // Méthode pour désactiver le mode sombre
+    private void disableDarkMode() {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        getDelegate().applyDayNight();
+        SharedPreferences preferences =
+                PreferenceManager.getDefaultSharedPreferences(this);
+        preferences.edit().putInt("night_mode", AppCompatDelegate.MODE_NIGHT_NO).apply();
     }
 }
