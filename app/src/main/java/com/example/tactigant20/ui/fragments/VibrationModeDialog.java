75fdb1c8fe881ebb14fc5b086d562f9538e80c77
Mathis Guckert <guckert.mathis@gmail.com>
2023-01-03 11:19:32 +0100

    Modified the VibrationModeDialog style  and solved bug regarding the CustomUIThread in the HomeFragment

--- app/src/main/java/com/example/tactigant20/ui/fragments/VibrationModeDialog.java
+++ app/src/main/java/com/example/tactigant20/ui/fragments/VibrationModeDialog.java
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.AdapterView;
